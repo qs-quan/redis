42822fccc6da0ed87762e3c49e23b57800125c74
quanxuesen <1468414549@qq.com>
2020-11-11 15:45:58 +0800

    redis 轻量级队列 空队列问题 延迟问题 通过bllop 阻塞读 解决 消费速度要 大于 生产速度 否则会 大量占用内存

--- src/main/java/com/redis/queue/Consumer.java
+++ src/main/java/com/redis/queue/Consumer.java
@@ -43,17 +43,8 @@ public class Consumer {
 
 
     public void comsumer1() {
-        /**
-         *     消息消费者有一个问题存在，即需要不停的调用lpop方法查看List中是否有待处理消息。
-         *    每调用一次都会发起一次连接，这会造成不必要的浪费。也许你会使用Thread.sleep()等方法让消费者线程隔一段时间再消费，但这样做有两个问题：
-         *
-         *         1）、如果生产者速度大于消费者消费速度，消息队列长度会一直增大，时间久了会占用大量内存空间。
-         *
-         *         2）、如果睡眠时间过长，这样不能处理一些时效性的消息，睡眠时间过短，也会在连接上造成比较大的开销。
-         *
-         *
-         */
         while (true) {
+            // 移出并获取列表的最后一个元素， 如果列表没有元素会阻塞列表直到等待超时或发现可弹出元素为止。
             String message = (String) redisTemplate.opsForList().leftPop(QUEUE_NAME,10,TimeUnit.SECONDS);
             System.out.println("comsumer1 + 读取完成==========");
             if(message != null){
